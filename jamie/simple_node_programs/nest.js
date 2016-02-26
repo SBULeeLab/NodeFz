84233e44a83014d7c9cd1700a33ee6b2d8651218
Jamie Davis <davisjam@vt.edu>
2016-02-26 18:53:48 -0500

    Sample progs: nest.js
    This executes four FS operations in a waterfall paradigm.

---
+++ jamie/simple_node_programs/nest.js
@@ -0,0 +1,19 @@
+/*
+  Let's watch the inheritance graph when an async callback
+  launches an async callback.
+*/
+
+var fs = require("fs");
+
+fs.writeFile("/tmp/foo", "AAAAAA", function(){
+  fs.readFile("/tmp/foo", function(){
+    fs.writeFile("/tmp/foo", "AAAAAAA", function(){
+      fs.readFile("/tmp/foo", function() { 
+        console.log("Bottomed out");
+      });
+    });
+  });
+});
+
+//Start reading from stdin so we don't exit.
+process.stdin.resume();
