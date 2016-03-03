bd502f8898cf9c333136577a3a6c014c5f985b23
Jamie Davis <davisjam@vt.edu>
2016-03-03 11:13:18 -0500

    dns_lookup: tweaks to discover SIGWINCH handler

--- jamie/simple_node_programs/dns_lookup.js
+++ jamie/simple_node_programs/dns_lookup.js
@@ -1,11 +1,26 @@
 /*
-  Exercise dns.lookup, which ultimately calls uv_getaddrinfo
+  Exercise dns.lookup, which ultimately calls uv_getaddrinfo.
+  Three paths in the CB make the source of the signal handler for SIGWINCH obviuos.
 */
 
 var dns = require('dns');
+var fs = require('fs');
 
 dns.lookup('www.google.com', { family: 6 }, function onLookup(err, addresses, family) {
-  console.log("www.google.com -> ", addresses);
+  var out = "www.google.com -> " + addresses;
+  if (1)
+  {
+    /* This registers a signal handler for SIGWINCH. */
+    console.log(out);
+  }
+  else if (0)
+  {
+    /* This does not. */
+    fs.writeFile('/tmp/foo', out);
+  }
+  else
+  {
+  }
 });
 
 //Start reading from stdin so we don't exit.
