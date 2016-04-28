3c6b9bdfbcc82b61b0797add26ba3023404b7620
Jamie Davis <davisjam@vt.edu>
2016-04-28 17:49:06 -0400

    timespec_funcs: might need these someday
    Subtraction

---
+++ deps/uv/src/timespec_funcs.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+
+/* RES = STOP - START. 
+   STOP must be after START. */
+void timespec_sub (const struct timespec *stop, const struct timespec *start, struct timespec *res)
+{
+  assert(stop);
+  assert(start);
+  assert(res);
+
+  /* START < STOP-> */
+  assert(start->tv_sec < stop->tv_sec || start->tv_nsec <= stop->tv_nsec);
+
+  if (stop->tv_nsec < start->tv_nsec)
+  {
+    /* Borrow. */
+    res->tv_nsec = 1000000000 + stop->tv_nsec - start->tv_nsec; /* Inline to avoid overflow. */
+    res->tv_sec = stop->tv_sec - start->tv_sec - 1;
+  }
+  else
+  {
+    res->tv_nsec = stop->tv_nsec - start->tv_nsec;
+    res->tv_sec = stop->tv_sec - start->tv_sec;
+  }
+
+  return;
+}
---
+++ deps/uv/src/timespec_funcs.h
@@ -0,0 +1,6 @@
+#ifndef UV_TIMESPEC_FUNCS_H_
+#define UV_TIMESPEC_FUNCS_H_
+
+void timespec_sub (const struct timespec *stop, const struct timespec *start, struct timespec *res)
+
+#endif /* UV_TIMESPEC_FUNCS_H_ */
