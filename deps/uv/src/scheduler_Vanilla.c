SBULeeLab/NodeFz

commit db84f5afbc9fa20e7986da1f211bc10db17d58ea

Timer fuzzing: update vanilla sched with defaults

deps/uv/src/scheduler_Vanilla.c
@@ -69,6 +69,8 @@ scheduler_vanilla_thread_yield (schedule_point_t point, void *pointDetails)
 {
   /* SPDs with complex inputs/outputs to modify. */
   spd_iopoll_before_handling_events_t *spd_iopoll_before_handling_events = NULL;
+  spd_timer_run_t *spd_timer_run = NULL;
+  spd_timer_next_timeout_t *spd_timer_next_timeout = NULL;
 
   int i = 0;
 
@@ -98,6 +100,24 @@ scheduler_vanilla_thread_yield (schedule_point_t point, void *pointDetails)
     case SCHEDULE_POINT_LOOPER_GETTING_DONE:
       assert(scheduler__get_thread_type() == THREAD_TYPE_LOOPER);
       ((spd_getting_done_t *) pointDetails)->index = 0;
+      break;
+    case SCHEDULE_POINT_TIMER_RUN:
+      assert(scheduler__get_thread_type() == THREAD_TYPE_LOOPER);
+      spd_timer_run = (spd_timer_run_t *) pointDetails;
+      /* Run timers if they've expired. */
+      if (spd_timer_run->timer->timeout < spd_timer_run->now)
+        spd_timer_run->run = 1;
+      else
+        spd_timer_run->run = 0;
+      break;
+    case SCHEDULE_POINT_TIMER_NEXT_TIMEOUT:
+      assert(scheduler__get_thread_type() == THREAD_TYPE_LOOPER);
+      spd_timer_next_timeout = (spd_timer_next_timeout_t *) pointDetails;
+      if (spd_timer_next_timeout->timer->timeout < spd_timer_run->now)
+        /* Already expired. */
+        spd_timer_next_timeout->time_until_timer = 0;
+      else
+        spd_timer_next_timeout->time_until_timer = spd_timer_next_timeout->timer->timeout - spd_timer_next_timeout->now;
       break;
     default:
       /* Nothing to do. */

commit 2d69303dae96d8d86ef1f6b35532f3fada14574a

Vanilla scheduler: fix null pointer bug
This issue was introduced in db84f5afbc9.

deps/uv/src/scheduler_Vanilla.c
@@ -113,7 +113,7 @@ scheduler_vanilla_thread_yield (schedule_point_t point, void *pointDetails)
     case SCHEDULE_POINT_TIMER_NEXT_TIMEOUT:
       assert(scheduler__get_thread_type() == THREAD_TYPE_LOOPER);
       spd_timer_next_timeout = (spd_timer_next_timeout_t *) pointDetails;
-      if (spd_timer_next_timeout->timer->timeout < spd_timer_run->now)
+      if (spd_timer_next_timeout->timer->timeout < spd_timer_next_timeout->now)
         /* Already expired. */
         spd_timer_next_timeout->time_until_timer = 0;
       else